f1aeb457162766439700e6561227397c1cd6c087
Luke Runco <lukerunco@luke.local>
2026-03-26 22:12:02 -0400

    Add built frontend

---
+++ client/dist/sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'apex-v1'
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/manifest.json'])))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  // Always pass API calls through to the network
+  if (e.request.url.includes('/api/')) return
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
